30f196faf95abbafa90cb953d87188b384a812b4
eveilleo <leo.eveille@univ-rouen.fr>
2024-02-01 14:15:24 +0100

    Modification TP10

--- TP10 - client http/Client3.java
+++ TP10 - client http/Client3.java
@@ -13,54 +13,91 @@ public class Client3
 	{
 		try 
 		{
-			CloseableHttpClient client = HttpClients.createDefault();
-			String url = "http://" + args[0];
-			HttpGet request = new HttpGet(url);
-			System.out.println("Executing request" + request.getRequestLine() );
-			CloseableHttpResponse resp = client.execute(request);
-			System.out.println("Response Line: " + resp.getStatusLine() );
-			System.out.println("Response Code: " + resp.getStatusLine().getStatusCode() );
-			InputStreamReader isr = new InputStreamReader(resp.getEntity().getContent() );
-			JsonReader reader = Json.createReader(isr);
-			JsonObject jsonObject = reader.readObject();
-			System.out.println("duree: " + jsonObject.getString("Runtime") );
-			JsonArray tab = jsonObject.getJsonArray("Ratings");
-			for (int i = 0; i < tab.size(); i++) {
-				JsonObject ji = tab.getJsonObject(i);
-				if ("Rotten Tomatoes".equals(ji.getString("Source"))) {
-					String rottenTomatoesValue = ji.getString("Value");
-					System.out.println("Rotten Tomatoes Value: " + rottenTomatoesValue);
-					displayScoreMention(rottenTomatoesValue);
+			while (true) 
+			{
+				BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+				System.out.print("Entrez le nom du film (ou 'exit' pour quitter) : ");
+				String title = reader.readLine();
+				if ("exit".equalsIgnoreCase(title)) 
+				{
+					System.out.println("Programme quitter.");
+					break;
 				}
+				CloseableHttpClient client = HttpClients.createDefault();
+
+				String url = "http://" + args[0] + title;
+				HttpGet request = new HttpGet(url);
+
+				System.out.println("Executing request" + request.getRequestLine() );
+				CloseableHttpResponse resp = client.execute(request);
+
+				System.out.println("Response Line: " + resp.getStatusLine() );
+				System.out.println("Response Code: " + resp.getStatusLine().getStatusCode() );
+
+				InputStreamReader isr = new InputStreamReader(resp.getEntity().getContent() );
+				JsonReader jsonReader = Json.createReader(isr);
+				JsonObject jsonObject = jsonReader.readObject();
+				
+				String releaseDate = getJsonValue(jsonObject, "Released");
+				System.out.println("Date de sortie : " + releaseDate);
+
+				String actors = getJsonValue(jsonObject, "Actors");
+				System.out.println("Acteurs principaux : " + actors);
+
+				JsonArray tab = getJsonArrayValue(jsonObject, "Ratings");
+				String rottenTomatoesScore = "N/A";
+
+				for (int i = 0; i < tab.size(); i++) {
+					JsonObject ji = tab.getJsonObject(i);
+					if ("Rotten Tomatoes".equals(ji.getString("Source"))) {
+						String rottenTomatoesValue = ji.getString("Value");
+					}
+				}
+
+				System.out.println("Score Rotten Tomatoes : " + rottenTomatoesScore);
+
+				int numericScore = parseNumericScore(rottenTomatoesScore);
+				String critiqueMention = getCritiqueMention(numericScore);
+				System.out.println("Critique : " + critiqueMention);
+
+				jsonReader.close();
+				isr.close();
 			}
-			reader.close();
-			isr.close();
-		}
+		}		
 		catch( Exception ex )
 		{
 			System.out.println( "erreur !" );
 			ex.printStackTrace();
 		}
 	}
-	private static void displayScoreMention(String score) 
-	{
-		try 
-		{
-			double scoreValue = Double.parseDouble(score.replaceAll("[^0-9]", ""));
-			System.out.print("Score Mention: ");
-			if (scoreValue < 20) {
-				System.out.println("Nul");
-			} else if (scoreValue >= 20 && scoreValue < 50) {
-				System.out.println("Bof");
-			} else if (scoreValue >= 50 && scoreValue < 70) {
-				System.out.println("Bien");
-			} else {
-				System.out.println("Très bien");
-			}
-		} 
-		catch (NumberFormatException e) 
-		{
-			System.out.println("Score non disponible ou mal formaté.");
+	private static String getJsonValue(JsonObject jsonObject, String key) {
+		return jsonObject.containsKey(key) && !jsonObject.isNull(key) ?
+			jsonObject.getString(key) :
+			"Non disponible";
+	}
+	private static JsonArray getJsonArrayValue(JsonObject jsonObject, String key) {
+		return jsonObject.containsKey(key) && !jsonObject.isNull(key) ?
+			jsonObject.getJsonArray(key) :
+			Json.createArrayBuilder().build();
+	}
+	private static int parseNumericScore(String score) {
+		try {
+			return Integer.parseInt(score.replaceAll("[^0-9]", ""));
+		} catch (NumberFormatException e) {
+			return -1; // Retourne -1 en cas d'échec de la conversion
+		}
+	}
+	private static String getCritiqueMention(int score) {
+		if (score < 0) {
+			return "Non disponible";
+		} else if (score < 20) {
+			return "Nul";
+		} else if (score <= 50) {
+			return "Bof";
+		} else if (score <= 70) {
+			return "Bien";
+		} else {
+			return "Très bien";
 		}
 	}
 }	
